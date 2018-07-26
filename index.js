728d0e3ca3b0121bb88bf850d9b5b9e15076fbc7
Marissa Faulkner <marissagrace808@gmail.com>
2018-07-25 19:27:40 -0700

    post-mentor sesh, added movie suggestions and video to DOM

--- index.js
+++ index.js
@@ -2,26 +2,77 @@
 
 // tastedive api key = 314781-MarissaF-SHF4KEGS
 
-const TASTEDIVE_ENDPOINT_URL = 'https://tastedive.com/api/similar';
+const TASTEDIVE_ENDPOINT_URL = 'http://tastedive.com/api/similar';
 
 
 function getDataFromTasteDiveApi(searchTerm, callback) {
   const settings = {
     url: TASTEDIVE_ENDPOINT_URL,
     data: {
-      q: `${searchTerm} in:name`,
+      q: searchTerm,
       type: 'movies',
       info: 1,
       limit: 20,
-      k: '314781-MarissaF-SHF4KEGS',
-      callback: callback
+      k: '314781-MarissaF-SHF4KEGS'
     },
-    dataType: 'json',
+    crossDomain: true,
+    dataType: 'jsonp',
     type: 'GET',
     success: callback
   };
 
   $.ajax(settings);
+}
+
+
+
+function renderMovieVideo(result) {
+	console.log('test', result)
+	return `
+	<div>
+		<iframe id="inlineFrameExample"
+    		title="Inline Frame Example"
+   			 width="300"
+   			 height="200"
+   			 src="${result.Similar.Info[0].yUrl}">
+		</iframe>
+	</div>
+	`
+}
+
+
+function displayMovieVideo(data) {
+	$('.js-video').html(renderMovieVideo(data));
+	console.log('test', data)
+}
+
+
+
+function renderResult(result) {
+  return `
+    <div>
+      <p>${result.Name}</p>
+      <p>${result.wTeaser}</p>
+    </div>
+  `;
+}
+
+
+
+function displayTasteDiveSearchData(data) {
+	console.log(data)
+  const movieResults = data.Similar.Results.map((item, index) => renderResult(item));
+
+  $('.js-search-results').html(movieResults);
+
+
+}
+
+
+function displayAllResults(data) {
+	displayTasteDiveSearchData(data);
+	displayMovieVideo(data);
+
 }
 
 
@@ -33,7 +84,9 @@ function watchSubmit() {
     const query = queryTarget.val();
     // clear out the input
     queryTarget.val("");
-    getDataFromApi(query, displayGitHubSearchData);
+
+    getDataFromTasteDiveApi(query, displayAllResults);
+
   });
 }
 
